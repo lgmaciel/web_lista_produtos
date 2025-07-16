1c342a3807e9f8fc986f3faca82ab2d813cb8001
lgmaciel <lgmaciel@email>
2025-07-16 15:38:04 -0300

    lista estática de produtos

--- app.py
+++ app.py
@@ -2,16 +2,37 @@ import flask
 
 app = flask.Flask(__name__)
 
+lista_de_produtos = [
+    (
+        'https://www.freepnglogos.com/uploads/notebook-png/download-laptop-notebook-png-image-png-image-pngimg-2.png',
+        '150,00',
+        'Coisa 1'
+    ),
+
+    (
+        'https://www.freepnglogos.com/uploads/notebook-png/download-laptop-notebook-png-image-png-image-pngimg-2.png',
+        '250,00',
+        'Coisa 2'
+    ),
+
+    (
+        'https://www.freepnglogos.com/uploads/notebook-png/download-laptop-notebook-png-image-png-image-pngimg-2.png',
+        '350,00',
+        'Coisa 3'
+    ),
+
+    (
+        'https://www.freepnglogos.com/uploads/notebook-png/download-laptop-notebook-png-image-png-image-pngimg-2.png',
+        '450,00',
+        'Coisa 4'
+    )
+
+]
+
 @app.get('/')
 def get_home():
-    img = 'https://www.freepnglogos.com/uploads/notebook-png/download-laptop-notebook-png-image-png-image-pngimg-2.png'
-    preco = '150,00'
-    nome = 'Coisa'
-
-    return flask.render_template("lista_produtos.html",
-                                img_produto = img,
-                                preco_produto = preco,
-                                nome_produto = nome)
+   
+    return flask.render_template("lista_produtos.html", produtos=lista_de_produtos)
 
 
 app.run(host='0.0.0.0', debug=True)
